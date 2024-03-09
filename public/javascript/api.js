fb29c8763218aea168bfbb10d4f8dc80fdd6a3d9
grecolive <48304759+grecolive@users.noreply.github.com>
2024-03-09 16:26:52 -0400

    new desing portfolio gsantos

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,22 +1,26 @@
-let btnContent = document.querySelector(".btntoContent");
+let navLink = document.querySelectorAll(".nav-link");
 let topButton = document.querySelector(".top");
 let copyCustomSpan = document.querySelector(".year");
 let currentYear = new Date().getFullYear();
 
-
-let fecha = calculate_age(new Date(1990, 10, 24));
-
-$(function() {
-    $('[data-toggle="tooltip"]').tooltip()
-});
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 copyCustomSpan.innerText = currentYear;
-btnContent.addEventListener("click", () => {
-    $('html, body').animate({
-        scrollTop: $(".educacion").offset().top - 50
-    }, 2000);
+
+// Desplazamiento suave
+navLink.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        let target = link.getAttribute("href");
+        document.getElementById(target).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
 });
 
+// Botón de desplazamiento superior
 window.onscroll = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         topButton.style.display = 'block';
@@ -25,23 +29,100 @@ window.onscroll = () => {
     }
 }
 
-topButton.addEventListener("click", () => {
-    $('html, body').animate({
-        scrollTop: $("header").offset().top - 50
-    }, 2000);
+// Cuando el usuario hace clic en el botón, desplácese hacia la parte superior de la página
+topButton.addEventListener("click", function() {
+    // Desplaza suavemente hacia la parte superior de la página
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
 
-function calculate_age(p_Fecha) {
+// Función para calcular la edad
+const calculate_age = (p_Fecha) => {
     let diff_ms = Date.now() - p_Fecha.getTime();
     let age_dt = new Date(diff_ms);
 
     return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
+
+// Calcular la edad
+let fecha = calculate_age(new Date(1990, 10, 24));
 //document.querySelector('.age').innerHTML = fecha;
 
+// Cambiar el color tema
+(() => {
+    'use strict';
   
+    const getStoredTheme = () => localStorage.getItem('theme');
+    const setStoredTheme = theme => localStorage.setItem('theme', theme);
+    const changeThemeDropdown = document.querySelector('.dropdown');
+  
+    // Get the user-preferred theme
+    const getPreferredTheme = () => {
+      const storedTheme = getStoredTheme();
+      if (storedTheme) {
+        return storedTheme;
+      }
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    };
+  
+    // Set the theme to the user-preferred theme
+    const setTheme = theme => {
+      if (theme === 'auto') {
+        document.documentElement.setAttribute('data-bs-theme', window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+        changeThemeDropdown.setAttribute('data-bs-theme', window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+        let dropdownChilds = changeThemeDropdown.childNodes[3].querySelectorAll(".dropdown-item");
+        dropdownChilds.forEach((child) => {
+            child.classList.remove('active');
+            if (child.getAttribute('data-bs-theme-value') === 'auto') {
+                child.classList.add('active');
+            }else{
+                child.classList.remove('active');
+                child.closest('.dropdown').firstChild.nextSibling.innerHTML = "";
+                let icon = child.firstChild.nextSibling.cloneNode(true);
+                child.closest('.dropdown').firstChild.nextSibling.appendChild(icon);
+            }
+        });
+    
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+        changeThemeDropdown.setAttribute('data-bs-theme', window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+      }
+    };
+
+    setTheme(getPreferredTheme());
+
+    // Set the theme to the user-preferred theme
+    document.querySelectorAll('[data-bs-theme-value]').forEach(themeOption => {
+        themeOption.addEventListener('click', () => {
+            const selectedTheme = themeOption.getAttribute('data-bs-theme-value');
+            themeOption.classList.add('active');
+
+            if (selectedTheme === 'auto') {
+                setStoredTheme('');
+            }
+
+            themeOption.closest('.dropdown').firstChild.nextSibling.innerHTML = "";
+            let icon = themeOption.firstChild.nextSibling.cloneNode(true);
+            themeOption.closest('.dropdown').firstChild.nextSibling.appendChild(icon);
+            setStoredTheme(selectedTheme);
+            setTheme(selectedTheme);
+        
+            // Remove active class from other theme options
+            document.querySelectorAll('[data-bs-theme-value]').forEach(el => {
+                if (el !== themeOption) {
+                    el.classList.remove('active');
+                }
+            });
+        });
+    });
+  })
+();
+  
+// Registrar el Service Worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('Registro de SW exitoso', reg))
-      .catch(err => console.warn('Error al tratar de registrar el sw', err))
-  }
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
